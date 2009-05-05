3c78f8f5437376afdc9e49fef352355113f50a64
Christopher Brady <cbrady11@gmail.com>
2009-05-05 17:54:28 -0400

    working on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :product_attributes
   has_many :product_options
+  has_many :in_stock_options, :class_name => 'ProductOption', :conditions => "quantity > 0"
   has_many :product_images
   
   named_scope :find_featured, :conditions => 'is_featured IS true AND active IS true'
